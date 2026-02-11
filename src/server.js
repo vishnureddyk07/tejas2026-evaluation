c91803185840560e6fb9d40fbc6cc9ad6324eaa1
vishnu vardhan reddy <vishnureddy.au07@gmail.com>
2026-02-11 16:20:04 +0530

    Add startup debug logging for ADMIN_USERS environment variable

--- src/server.js
+++ src/server.js
@@ -3,6 +3,8 @@ import config from "./config/index.js";
 import { initDb } from "./db/index.js";
 
 const startServer = async () => {
+  console.log("DEBUG: Startup - ADMIN_USERS env:", process.env.ADMIN_USERS);
+  console.log("DEBUG: Startup - config.adminUsers:", config.adminUsers);
   await initDb();
   app.listen(config.port, () => {
     console.log(`TEJUS 2026 server running on port ${config.port}`);
